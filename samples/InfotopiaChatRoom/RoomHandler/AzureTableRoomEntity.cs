d2436d2f30cc6ba40d27dc8ecefdd32a9983048b
Keith <keithazzopardi.development@gmail.com>
2023-03-12 04:15:02 +0100

    entity skeletons

---
+++ samples/InfotopiaChatRoom/RoomHandler/AzureTableRoomEntity.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace Microsoft.Azure.SignalR.Samples.InfotopiaChatRoom
+{
+    public class RoomEntity : TableEntity
+    {
+        
+    }
+}
